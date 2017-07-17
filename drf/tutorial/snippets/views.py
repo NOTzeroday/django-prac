7e6f0e6f3899ab368d37d1232ed6b24200b0cf31
NOTzeroday <kenneth.murugu@savannahinformatics.com>
2017-07-17 13:31:39 +0300

    4_1_endtut4

--- drf/tutorial/snippets/views.py
+++ drf/tutorial/snippets/views.py
@@ -2,21 +2,23 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-#from django.http import HttpResponse, JsonResponse
+# from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 from django.http import Http404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import mixins, generics
-
+from django.contrib.auth.models import User
+from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
 
 # Create your views here.
-#class-based views
+# class-based views
 """
 class SnippetList(mixins.ListModelMixin,
                   mixins.CreateModelMixin,
@@ -50,10 +52,28 @@ class SnippetDetail(mixins.RetrieveModelMixin,
         return self.destroy(request, *args, **kwargs)
 """
 
+
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
--- drf/tutorial/tutorial/urls.py
+++ drf/tutorial/tutorial/urls.py
@@ -21,4 +21,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^snippets/', include('snippets.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
